ce13e5e586fd59aad041a8270d07b2fae5239b74
hani <haniyousfi@gmail.com>
2023-11-24 06:03:45 +0100

    add new files

---
+++ src/pages/_app.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import "../styles/global.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "400", "300", "500", "600", "700"],
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${poppins.variable} font-poppins`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
