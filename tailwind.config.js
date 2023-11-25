0d459c4ff6070002ff51164d73a3c87b8586ce64
hani <haniyousfi@gmail.com>
2023-11-25 22:59:17 +0100

    configure CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ module.exports = {
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
+        white: "#fff",
+        light_purple: "#8490ff"
       },
     },
   },
