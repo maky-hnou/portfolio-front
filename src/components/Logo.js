00423f4b8a3eb8bdb2fba4b53f8f921daefcef23
hani <haniyousfi@gmail.com>
2023-11-28 01:06:48 +0100

    add shadow to logo text

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,19 +3,35 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const MotionLink = motion(Link);
+
+const TextWithLetterShadow = ({ children }) => {
+    const singleLetterVariant = {
+    initial: { textShadow: "none" },
+    hover: { textShadow: "0px 5px 8px #8490ff" },
+  };
+
+  return (
+    <motion.span whileHover="hover">
+      {Array.from(children).map((letter, index) => (
+        <motion.span key={index} variants={singleLetterVariant}>
+          {letter}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+};
+
 export default function Logo() {
   return (
     <div className="flex items-start justify-start">
       <MotionLink
         href="/"
-        className="w-max ml-8 px-2 flex text-light_purple items-start justify-start text-2xl border-2 border-solid border-light_purple rounded-full"
+        className="w-max ml-8 px-2 flex text-light_purple items-start justify-start text-4xl font-medium"
         whileHover={{
           scale: 1.2,
-          backgroundColor: "#8490ff",
-          color: "#fff",
         }}
       >
-        H.Yousfi
+        <TextWithLetterShadow>H.Yousfi</TextWithLetterShadow>
       </MotionLink>
     </div>
   );
