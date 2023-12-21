69cbb13fc69c6c5d19860e0e654426ff4f6fa6b5
hani <haniyousfi@gmail.com>
2023-12-22 00:27:35 +0100

    fix text width with motion effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,14 @@ import Image from "next/image";
 import profilepicture from "../../media/hani.jpg";
 import AnimatedText from "../components/AnimatedText";
 import AnimatedCharacters from "../components/AnimatedCharacters";
+import TextSplitter from "../components/TextSplitter";
 
 export default function Home() {
+  const textAreaWidth = 800;
+  const sampleText = `Dedicated contemplative coder and astute analyst, fueled by a passion for tackling intricate challenges.
+    Passionate about unraveling the intricacies of Data, crafting Complex Adaptive Systems and the world of Cognitive Modeling.
+    Thriving on the opportunities to apply solid analytical prowess and coding skills when facing convoluted problems.`;
+  const lines = TextSplitter({ text: sampleText, maxWidth: textAreaWidth });
   return (
     <div>
       <Head>
@@ -20,22 +26,14 @@ export default function Home() {
               className="flex mb-5 mt-3 text-5xl font-semibold space-x-1"
             />
 
-            <AnimatedCharacters
-              text="Dedicated contemplative coder and astute analyst, fueled by a
-            passion for tackling intricate challenges."
-              className="text-text_color mb-2 overflow-hidden flex"
-            />
-            <AnimatedCharacters
-              text="Passionate about unraveling the intricacies of Data, crafting
-              Complex Adaptive Systems and the world of Cognitive Modeling."
-              reverse
-              className="text-text_color mb-2 overflow-hidden flex"
-            />
-            <AnimatedCharacters
-              text="Thriving on the opportunities to apply solid analytical prowess
-              and coding skills when facing convoluted problems."
-              className="text-text_color mb-2 overflow-hidden flex"
-            />
+            {lines.map((line, index) => (
+              <AnimatedCharacters
+                key={index}
+                text={line}
+                reverse={index % 2 === 1} // Reverse every other line
+                className="text-text_color mb-2 overflow-hidden flex"
+              />
+            ))}
           </div>
           <div className="w-80 h-80">
             <Image
