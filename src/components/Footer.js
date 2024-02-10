4d5428cee2d293040702c12b287792ebee9eebce
hani <haniyousfi@gmail.com>
2024-02-10 23:46:57 +0100

    Add footer and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import { EmailIcon, GitHubIcon, LinkedInIcon } from "./Icons";
 
 export default function Footer() {
   return (
     <footer className="bg-black p-20 box-border">
       <div className="max-w-[960px] w-full flex flex-wrap border-box mx-auto px-4 text-white text-base font-light">
-        <div>
+        <div className="mx-auto">
           <h4>About This page</h4>
           <p>
             Inspired from{" "}
@@ -21,8 +22,22 @@ export default function Footer() {
             Copyright &copy; {new Date().getFullYear()} All rights reserved.
           </p>
         </div>
-        <div>Contact</div>
-        <div>Follow Me</div>
+        <div className="mx-auto">
+          <h4>Contact</h4>
+          <div className="flex items-center content-center">
+            <Link href="mailto:haniyousfi@gmail.com">
+              <EmailIcon />
+            </Link>
+            <p className="m-auto ml-2">haniyousfi@gmail.com</p>
+          </div>
+        </div>
+        <div className="mx-auto">
+          <h4>Follow Me On</h4>
+          <div className="flex items-center content-center">
+            <LinkedInIcon />
+            <GitHubIcon />
+          </div>
+        </div>
       </div>
     </footer>
   );
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -15,7 +15,6 @@ export function DEIcon({ className, ...rest }) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
         style={{
           enableBackground: "new 0 0 96 96",
         }}
@@ -44,7 +43,6 @@ export function SEIcon({ className, ...rest }) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
         viewBox="0 0 96 120"
         className={`w-24 h-24 ${className}`}
         {...rest}
@@ -137,7 +135,6 @@ export function AIIcon({ className, ...rest }) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
         viewBox="0 0 100 125"
         className={`w-24 h-24 ${className}`}
         {...rest}
@@ -165,7 +162,6 @@ export function DSIcon({ className, ...rest }) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        xmlSpace="preserve"
         viewBox="0 0 512 640"
         className={`w-24 h-24 ${className}`}
         {...rest}
@@ -179,6 +175,89 @@ export function DSIcon({ className, ...rest }) {
         />
       </svg>
       <h4 className="mb-5 text-lg font-semibold">Data Science</h4>
+    </motion.div>
+  );
+}
+
+export function EmailIcon({ className, ...rest }) {
+  return (
+    <motion.div
+      className="mx-auto w-fit"
+      whileHover={{
+        fill: "#8490ff",
+        color: "#8490ff",
+      }}
+      initial={{
+        fill: "currentColor",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        style={{
+          shapeRendering: "geometricPrecision",
+          textRendering: "geometricPrecision",
+          imageRendering: "optimizeQuality",
+        }}
+        viewBox="19.64 53.07 294.03 227.19"
+        className={`w-8 h-8 ${className}`}
+        {...rest}
+      >
+        <path
+          d="M43.3 53.07l246.72 0c13,0 23.64,10.64 23.64,23.65l0 2.83 -111.68 97.5c-10.17,8.88 -22.74,13.31 -35.33,13.31 -12.58,0 -25.16,-4.44 -35.33,-13.31l-111.68 -97.5 0 -2.83c0,-13.01 10.64,-23.65 23.64,-23.65zm270.37 46.3l0 157.25c0,13 -10.64,23.64 -23.64,23.64l-246.72 0c-13,0 -23.64,-10.64 -23.64,-23.64l0 -157.25 101.84 88.91c13.03,11.38 29.1,17.06 45.16,17.06 16.06,0 32.13,-5.69 45.16,-17.06l101.84 -88.91z"
+          className="fil0"
+        />
+      </svg>
+    </motion.div>
+  );
+}
+
+export function GitHubIcon({ className, ...rest }) {
+  return (
+    <motion.div
+      className="mx-auto w-fit"
+      whileHover={{
+        fill: "#8490ff",
+        color: "#8490ff",
+      }}
+      initial={{
+        fill: "currentColor",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 30 30"
+        className={`w-10 h-10 mt-2 ${className}`}
+        {...rest}
+      >
+        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+      </svg>
+    </motion.div>
+  );
+}
+
+export function LinkedInIcon({ className, ...rest }) {
+  return (
+    <motion.div
+      className="mx-auto w-fit"
+      whileHover={{
+        fill: "#8490ff",
+        color: "#8490ff",
+      }}
+      initial={{
+        fill: "currentColor",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 30 30"
+        className={`w-10 h-10 ${className}`}
+        {...rest}
+      >
+        <path d="M9 25H4V10h5v15zM6.501 8a2.5 2.5 0 1 1 0-5.001A2.5 2.5 0 0 1 6.5 8zM27 25h-4.807v-7.3c0-1.741-.033-3.98-2.499-3.98-2.503 0-2.888 1.896-2.888 3.854V25H12V9.989h4.614v2.051h.065c.642-1.18 2.211-2.424 4.551-2.424 4.87 0 5.77 3.109 5.77 7.151V25z" />
+      </svg>
     </motion.div>
   );
 }
