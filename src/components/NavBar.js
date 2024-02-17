bc5b03446415a866463f889b90e3f0aef5d08804
hani <haniyousfi@gmail.com>
2024-02-17 22:50:19 +0100

    Add shadow on scroll for the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,34 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import { motion } from "framer-motion";
 
 const MotionLink = motion(Link);
 
 export default function NavBar() {
+  const [showShadow, setShowShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setShowShadow(scrollTop > 0);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="w-full py-8 flex items-center justify-between mt-2">
+    <header
+      className={`w-full py-9 flex items-center justify-between fixed bg-white ${
+        showShadow ? "shadow-[-21.213px_21.213px_30px_0px_#9e9e9e4d]" : ""
+      }`}
+    >
       <nav className="absolute right-0 font-medium text-xs uppercase">
         <MotionLink
           href="/"
