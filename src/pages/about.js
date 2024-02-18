355ab438f53e3be7cfd82ac2a80f70fbc9f5fb98
hani <haniyousfi@gmail.com>
2024-02-18 23:49:08 +0100

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>H. YOUSFI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-full h-auto p-14 bg-light_white text-black pt-32">
+        <div>
+          <h5>PERSONAL DETAILS</h5>
+        </div>
+      </main>
+    </div>
+  );
+}
