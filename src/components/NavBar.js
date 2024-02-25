b40b0103c7922d3d1d658cc632870fe185ea9d29
hani <haniyousfi@gmail.com>
2024-02-25 22:36:24 +0100

    Edit mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,53 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const MotionLink = motion(Link);
+
+export function CustomLink({ href, title, target, className = "" }) {
+  const router = useRouter();
+  console.log(router.asPath, href);
+
+  return (
+    <Link
+      href={href}
+      target={target ? target : undefined}
+      className={`${className} ${
+        router.asPath === href ? "text-light_purple" : "text-black"
+      } px-2 hover:text-light_purple`}
+    >
+      {title}
+    </Link>
+  );
+}
+
+export function CustomMobileLink({
+  href,
+  title,
+  target,
+  className = "",
+  toggle,
+}) {
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      target={target ? target : undefined}
+      className={`${className} ${
+        router.asPath === href ? "text-light_purple" : "text-white"
+      } py-4 uppercase hover:text-light_purple`}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+}
 
 export default function NavBar() {
   const [showShadow, setShowShadow] = useState(false);
@@ -57,90 +102,45 @@ export default function NavBar() {
       </div>
       <div className="w-full flex justify-between items-center lg:hidden">
         <nav className="absolute right-0 font-medium text-xs uppercase">
-          <MotionLink
-            href="/"
-            className="px-2"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            Home
-          </MotionLink>
-          <MotionLink
-            href="/about"
-            className="px-2"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            About
-          </MotionLink>
-          <MotionLink
-            href="/portfolio"
-            className="px-2"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            Portfolio
-          </MotionLink>
-          <MotionLink
+          <CustomLink href="/" title="Home" />
+          <CustomLink href="/about" title="About" />
+
+          <CustomLink href="/portfolio" title="Portfolio" />
+
+          <CustomLink
             href="media/Hani_Yousfi_Resume.pdf"
+            title="Résumé"
             target="_blank"
-            className="px-2 py-0.5 mr-32 ml-3 text-light_purple border-1 border-solid border-light_purple rounded-full"
-            whileHover={{
-              backgroundColor: "#8490ff",
-              color: "#fff",
-            }}
-          >
-            Resume
-          </MotionLink>
+            className="py-0.5 mr-32 ml-3 text-light_purple border-1 border-solid border-light_purple rounded-full hover:bg-light_purple hover:text-white"
+          />
         </nav>
       </div>
-      <div className="fixed right-0 top-0 pt-5 bg-black/[0.8] w-64 h-full transition-all duration-400 ease">
-        <nav className="flex flex-col mb-4 text-white font-medium text-xs uppercase">
-          <MotionLink
-            href="/"
-            className="px-2 py-4"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            Home
-          </MotionLink>
-          <MotionLink
-            href="/about"
-            className="px-2 py-4"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            About
-          </MotionLink>
-          <MotionLink
-            href="/portfolio"
-            className="px-2 py-4"
-            whileHover={{
-              color: "#8490ff",
-            }}
-          >
-            Portfolio
-          </MotionLink>
-          <div className="py-4">
-            <MotionLink
+
+      {isOpen ? (
+        <div className="fixed right-0 top-0 pt-5 bg-black/[0.8] w-64 h-full transition-all duration-400 ease">
+          <nav className="grid flex-col pt-8 pl-8 text-white font-medium text-xs justify-items-start">
+            <CustomMobileLink href="/" title="Home" toggle={handleClick} />
+
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              toggle={handleClick}
+            />
+
+            <CustomMobileLink
+              href="/portfolio"
+              title="Portfolio"
+              toggle={handleClick}
+            />
+            <CustomMobileLink
               href="media/Hani_Yousfi_Resume.pdf"
+              title="Résumé"
               target="_blank"
-              className="w-fit px-2 py-0.5 mr-32 text-white border-1 border-solid border-light_purple rounded-full"
-              whileHover={{
-                backgroundColor: "#8490ff",
-                color: "#fff",
-              }}
-            >
-              Resume
-            </MotionLink>
-          </div>
-        </nav>
-      </div>
+              toggle={handleClick}
+            />
+          </nav>
+        </div>
+      ) : null}
       <div className="absolute left-0">
         <Logo />
       </div>
