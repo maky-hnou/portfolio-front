dece1d7e4147bdb5b159a43b010700334a71c9eb
hani <haniyousfi@gmail.com>
2024-02-28 00:59:11 +0100

    Close mobile menu when click outside

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,10 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Logo from "../components/Logo";
-import { motion } from "framer-motion";
 import { useRouter } from "next/router";
-
-const MotionLink = motion(Link);
 
 export function CustomLink({ href, title, target, className = "" }) {
   const router = useRouter();
-  console.log(router.asPath, href);
 
   return (
     <Link
@@ -56,6 +52,20 @@ export default function NavBar() {
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setIsOpen(!isOpen);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -74,13 +84,13 @@ export default function NavBar() {
 
   return (
     <header
-      className={`w-full py-9 flex items-center justify-between fixed bg-white ${
+      className={`w-full py-9 flex items-center justify-between fixed bg-white z-0 ${
         showShadow ? "shadow-[-21.213px_21.213px_30px_0px_#9e9e9e4d]" : ""
       }`}
     >
-      <div className="hidden lg:flex">
+      <div className="hidden z-20 lg:flex">
         <button
-          className="flex-col justify-center items-center right-5 absolute"
+          className="flex-col justify-center items-center right-5 absolute z-10"
           onClick={handleClick}
         >
           <span
@@ -117,8 +127,11 @@ export default function NavBar() {
       </div>
 
       {isOpen ? (
-        <div className="fixed right-0 top-0 pt-5 bg-black/[0.8] w-64 h-full transition-all duration-400 ease">
-          <nav className="grid flex-col pt-8 pl-8 text-white font-medium text-xs justify-items-start">
+        <div
+          ref={menuRef}
+          className="w-64 h-full fixed right-0 top-0 pt-5 bg-black/[0.8] z-20 transition-all duration-400 ease"
+        >
+          <nav className="flex flex-col pt-8 pl-8 text-white font-medium text-xs items-start">
             <CustomMobileLink href="/" title="Home" toggle={handleClick} />
 
             <CustomMobileLink
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,6 @@ import { Poppins, Montserrat } from "next/font/google";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
-
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
@@ -24,7 +23,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${poppins.variable} font-poppins ${montserrat.variable} font-montserrat`}>
+      <main
+        className={`${poppins.variable} font-poppins ${montserrat.variable} font-montserrat`}
+      >
         <NavBar />
         <Component {...pageProps} />
         <Footer />
