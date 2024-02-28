dadc6ff7b170f565985184f591c902cbfb28f2f5
hani <haniyousfi@gmail.com>
2024-02-28 23:43:55 +0100

    Hide button when mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -88,28 +88,30 @@ export default function NavBar() {
         showShadow ? "shadow-[-21.213px_21.213px_30px_0px_#9e9e9e4d]" : ""
       }`}
     >
-      <div className="hidden z-20 lg:flex">
-        <button
-          className="flex-col justify-center items-center right-5 absolute z-10"
-          onClick={handleClick}
-        >
-          <span
-            className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
-              isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
-            }`}
-          ></span>
-          <span
-            className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
-              isOpen ? "opacity-0" : "opacity-100"
-            }`}
-          ></span>
-          <span
-            className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
-              isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
-            }`}
-          ></span>
-        </button>
-      </div>
+      {!isOpen && (
+        <div className="hidden z-20 lg:flex">
+          <button
+            className="flex-col justify-center items-center right-5 absolute z-10"
+            onClick={handleClick}
+          >
+            <span
+              className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
+                isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+              }`}
+            ></span>
+            <span
+              className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+                isOpen ? "opacity-0" : "opacity-100"
+              }`}
+            ></span>
+            <span
+              className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
+                isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+              }`}
+            ></span>
+          </button>
+        </div>
+      )}
       <div className="w-full flex justify-between items-center lg:hidden">
         <nav className="absolute right-0 font-medium text-xs uppercase">
           <CustomLink href="/" title="Home" />
