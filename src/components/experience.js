88c499d1113ccfb254a253802c4c6362d64c7859
hani <haniyousfi@gmail.com>
2024-04-14 18:37:03 +0200

    Add more data to about page

---
+++ src/components/experience.js
@@ -0,0 +1,117 @@
+export default function Experience() {
+  return (
+    <ul className="m-0 p-0 list-none list-outside list-image-none box-border">
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:left-[50px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">
+                Preparatory Diploma for Engineering Studies
+              </time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Faculty of Sciences of Monastir</b>
+            </p>
+            <p className="mb-[5px] text-center">September 2012 - June 2015</p>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">
+                Succeeding the National Competitive Exam
+              </b>
+            </p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:-left-[405px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">Masters in Network Engineering</time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">
+                National School of Electronics and Telecommunications of Sfax
+              </b>
+            </p>
+            <p className="mb-[5px] text-center">September 2015 - July 2018</p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:left-[50px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">Machine Learning Intern</time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Novel-Ti</b>
+            </p>
+            <p className="mb-[5px] text-center">February 2018 - July 2018</p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:-left-[405px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">
+                R&D Engineer - Artificial Intelligence
+              </time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Novel-Ti</b>
+            </p>
+            <p className="mb-[5px] text-center">September 2018 - July 2021</p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:left-[50px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">
+                IT Consultant & Machine Learning Engineer
+              </time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Aigometri AB</b>
+            </p>
+            <p className="mb-[5px] text-center">August 2021 - April 2022</p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:-left-[405px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">Data Engineer</time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Klarna Bank AB</b>
+            </p>
+            <p className="mb-[5px] text-center">May 2022 - May 2023</p>
+          </div>
+        </div>
+      </li>
+      <li className="relative mx-auto my-0 w-[2px] pb-10 list-none box-border">
+        <div className="z-10 content-[''] bg-light_purple absolute left-1/2 top-[40%] -translate-x-[43%] w-5 h-5 border-solid rounded-full border-[5px] box-border"></div>
+        <div className="left-1/2 bg-light_white relative opacity-100">
+          <div className="opacity-100 odd:left-[50px] bg-white shadow-[0_10px_27px_3px_rgba(204,204,204,0.2)] relative top-2 w-[360px] p-[30px] box-border">
+            <h4 className="pb-[10px] text-center text-lg text-black my-0 font-semibold box-border">
+              <time className="box-border">Sfotware Engineer</time>
+            </h4>
+            <p className="mb-[5px] text-center">
+              <b className="text-black">Klarna Bank AB</b>
+            </p>
+            <p className="mb-[5px] text-center">May 2023 - Present</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import profilepicture from "../../media/hani.jpg";
+import Experience from "../components/experience";
 
 export default function About() {
   return (
@@ -9,60 +10,69 @@ export default function About() {
         <title>H. YOUSFI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-full h-auto p-14 bg-light_white text-black pt-32">
+      <main className="w-full h-auto p-14 bg-white text-black pt-32">
         <div>
+          <div className="py-12">
+            <div className="flex items-center justify-between w-full">
+              <div className="w-80 h-80 md:mx-auto">
+                <Image
+                  src={profilepicture}
+                  alt="Hani"
+                  className="place-items-end float-right w-full h-full object-cover border-hidden rounded-full"
+                />
+              </div>
+              <div className="grow-0 shrink-0 basis-5/12 px-4 box-border">
+                <h1 className="mb-10 uppercase text-4xl text-black font-semibold">
+                  PERSONAL DETAILS
+                </h1>
+                <p className="mb-8 max-w-[445px] mt-0 text-text_color text-base text-left font-light">
+                  Here's some more info about me.
+                </p>
+              </div>
+            </div>
+            <div className="pt-16 w-full relative px-4 box-border text-text_color text-base font-light">
+              <p className="mt-0 mb-4 block">
+                I started as a Network Engineering Student. However, I was more
+                curious and eager to learn about Artificial Intelligence. So, I
+                ended up doing my graduation internship in Novel-Ti (a Tunisian
+                startup) and discovering more about AI and Computer Vision.
+              </p>
+              <p className="mt-0 mb-4 block">
+                In September 2018, I started working full-time at Novel-Ti as an
+                R&D Engineer where I helped building AI and Computer Vision
+                solutions that solve real world problems.
+              </p>
+              <p className="mt-0 mb-4 block">
+                In August 2021, I decided to start my first experience abroad.
+                My first role was a Machine Learning and IT consultant at
+                Aigometri AB. I assisted my colleagues with working on an
+                internal project where I took care of the AI & Computer Vision
+                part and helped in building a test platform for the project. I
+                also worked as a Software Engineer Consultant for one of the
+                biggest fintech companies in Europe. My role as consultant was
+                about maintaining integrations with third party systems and
+                building automations to improve the efficiency and performance
+                of the different teams.
+              </p>
+              <p className="mt-0 mb-4 block">
+                May 2022 was the beginning of my third work experience. I was
+                offered a Data Engineer role at Klarna. Since then, I discovered
+                my passion about Data; my role there covers data-modeling,
+                designing and building ETL pipelines and data-warehousing.
+              </p>
+            </div>
+          </div>
           <div>
+            <hr className="mb-20 mx-[10%] border-t-1" />
+          </div>
+          <div className="py-20 block box-border text-text_color font-light text-base">
+            <div className="text-center pb-10 ">
+              <h1 className="mb-2.5 text-4xl font-semibold mt-0 text-black">
+                My Experience
+              </h1>
+            </div>
             <div>
-              <div className="flex items-center justify-between w-full">
-                <div className="w-80 h-80 md:mx-auto">
-                  <Image
-                    src={profilepicture}
-                    alt="Hani"
-                    className="place-items-end float-right w-full h-full object-cover border-hidden rounded-full"
-                  />
-                </div>
-                <div className="grow-0 shrink-0 basis-5/12 px-4 box-border">
-                  <h1 className="mb-10 uppercase text-4xl text-black font-semibold">
-                    PERSONAL DETAILS
-                  </h1>
-                  <p className="mb-8 max-w-[445px] mt-0 text-text_color text-base text-left font-light">
-                    Here's some more info about me.
-                  </p>
-                </div>
-              </div>
-              <div className="pt-16 w-full relative px-4 box-border text-text_color text-base font-light">
-                <p className="mt-0 mb-4 block">
-                  I started as a Network Engineering Student. However, I was
-                  more curious and eager to learn about Artificial Intelligence.
-                  So, I ended up doing my graduation internship in Novel-Ti (a
-                  Tunisian startup) and discovering more about AI and Computer
-                  Vision.
-                </p>
-                <p className="mt-0 mb-4 block">
-                  In September 2018, I started working full-time at Novel-Ti as
-                  an R&D Engineer where I helped building AI and Computer Vision
-                  solutions that solve real world problems.
-                </p>
-                <p className="mt-0 mb-4 block">
-                  In August 2021, I decided to start my first experience abroad.
-                  My first role was a Machine Learning and IT consultant at
-                  Aigometri AB. I assisted my colleagues with working on an
-                  internal project where I took care of the AI & Computer Vision
-                  part and helped in building a test platform for the project. I
-                  also worked as a Software Engineer Consultant for one of the
-                  biggest fintech companies in Europe. My role as consultant was
-                  about maintaining integrations with third party systems and
-                  building automations to improve the efficiency and performance
-                  of the different teams.
-                </p>
-                <p className="mt-0 mb-4 block">
-                  May 2022 was the beginning of my third work experience. I was
-                  offered a Data Engineer role at Klarna. Since then, I
-                  discovered my passion about Data; my role there covers
-                  data-modeling, designing and building ETL pipelines and
-                  data-warehousing.
-                </p>
-              </div>
+              <Experience />
             </div>
           </div>
           // Put the next sections here
