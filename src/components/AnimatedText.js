d47be49bfd0a54cfcc47f90c70f088ed297b0c76
hani <haniyousfi@gmail.com>
2024-05-25 23:30:43 +0200

    Optimize animated AnimatedText

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -3,13 +3,11 @@ import { motion } from "framer-motion";
 import TextSpan from "./TextSpan";
 
 export default function AnimatedText({ text, className = "" }) {
-  return (
-    <h3 className={className}>
-      {text.split("").map((letter, index) => {
-        return (
-          <TextSpan key={index}>{letter === " " ? "\u00A0" : letter}</TextSpan>
-        );
-      })}
-    </h3>
-  );
+  const renderText = () => {
+    return text.split("").map((letter, index) => (
+      <TextSpan key={index}>{letter === " " ? "\u00A0" : letter}</TextSpan>
+    ));
+  };
+
+  return <h3 className={className}>{renderText()}</h3>;
 }
