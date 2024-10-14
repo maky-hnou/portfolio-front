0179346f95822db16cbe48a222f8bdb66f69549c
hani <haniyousfi@gmail.com>
2024-10-14 19:01:25 +0200

    Refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,12 +15,9 @@ import {
 } from "../components/Icons";
 import SkillSet from "../components/SkillSet";
 import IconCard from "../components/IconCard";
-import { Skills } from "../components/skills";
+import { Skills, sampleText } from "../components/text";
 
 export default function Home() {
-  const sampleText = `Dedicated contemplative coder and astute analyst, fueled by a passion for tackling intricate challenges.
-    Passionate about unraveling the intricacies of Data, crafting Complex Adaptive Systems and the world of Cognitive Modeling.
-    Thriving on the opportunities to apply solid analytical prowess and coding skills when facing convoluted problems.`;
 
   return (
     <div>
